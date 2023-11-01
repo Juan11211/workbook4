a2f988f9597d42cafa0e0f3321ac96cc10813412
Juan11211 <avilesjuan628@gmail.com>
2023-11-01 15:20:32 -0400

    more to come

---
+++ Sorting/sort_average.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let restOfClass = [83, 89, 90, 72, 86, 95];
+let myScore = [90, 94, 86, 88, 98, 93];
+
+function getAverage(avg) {
+    let sum = 0;
+// loop through each element and add each element[i] to the sum += 
+    for (let i = 0; i < avg.length; i++) {
+        sum += avg[i];
+    }
+
+    // return sum and divide by the avg length
+    return sum / avg.length;
+}
+
+function sortScores(a, b){
+    return a < b
+}
+
+function getMedian(score){
+
+}
